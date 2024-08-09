36f7d7f4300508547b43359ac6847e13bf62c03d
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-08-09 00:40:42 -0400

    Add missing-number

---
+++ problems/missing-number/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums_set = set(nums)
+        for i in range(n+1):
+            if i not in nums_set:
+                return i
